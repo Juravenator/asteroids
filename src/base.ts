6ee622c97125b4bebdac483728bd717f9292c659
Glenn Dirkx <glenn.dirkx@gmail.com>
2026-02-21 20:00:58 +0100

    display lives

--- src/base.ts
+++ src/base.ts
@@ -1,4 +1,18 @@
 import { canvas, context } from "./canvas";
+import * as score from "./score";
+
+const heart = new Path2D();
+heart.moveTo(7.5, 12.5);
+heart.lineTo(14.5, 5.5);
+heart.lineTo(14.5, 2.5);
+heart.lineTo(12.5, 0.5);
+heart.lineTo(9.5, 0.5);
+heart.lineTo(7.5, 2.5);
+heart.lineTo(5.5, 0.5);
+heart.lineTo(2.5, 0.5);
+heart.lineTo(0.5, 2.5);
+heart.lineTo(0.5, 5.5);
+heart.closePath();
 
 export const draw = () => {
     const w = canvas.width;
@@ -23,4 +37,9 @@ export const draw = () => {
     context.lineTo(w - 10.5, h - 5.5);
 
     context.stroke();
+    for (let i = 1; i <= score.data.lives; i++) {
+        context.resetTransform();
+        context.translate(w - 5 - (20 * i), 10);
+        context.stroke(heart);
+    }
 }
