a5298d3082677a49516591f6fb4bea8200dda75c
Glenn Dirkx <glenn.dirkx@gmail.com>
2026-02-22 15:25:27 +0100

    better builds

--- rollup.mjs
+++ rollup.mjs
@@ -1,11 +1,15 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 export default {
 	output: {
-		// format: 'iife',
-    format: 'es',
-    compact: false,
+		format: 'iife',
+    // format: 'es',
+    compact: true,
     sourcemap: false,
 	},
-  plugins: [nodeResolve()]
+  plugins: [
+    nodeResolve(),
+    terser()
+  ]
 };
